3bae63bd9033b1db68d4c809d18dbf74bfcda930
Dyw <dongyouweie@126.com>
2022-04-26 22:05:31 +0800

    第一节

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by DongYouWei on 2022/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            List(landmarks) { landmark in
+                NavigationLink(destination: Text("111")){
+                    HStack{
+                        Image(landmark.imageName)
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 60.0, height: 60.0)
+                            .clipped()
+                            .cornerRadius(8)
+                        VStack(alignment: .leading){
+                            Text(landmark.name)
+                            Text(landmark.location)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("世界地标")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by DongYouWei on 2022/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
