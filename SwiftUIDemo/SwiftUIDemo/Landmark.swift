63e0c8503aa4406de142f2e98c03ff1cf4521730
‘dongyouwei’ <329720990@qq.com>
2022-06-01 21:05:33 +0800

    fix

--- SwiftUIDemo/SwiftUIDemo/Landmark.swift
+++ SwiftUIDemo/SwiftUIDemo/Landmark.swift
@@ -27,6 +27,13 @@ let landmarks = [
     Landmark(name: "大召寺", enName: "Big called temple", location: "内蒙古呼和浩特"),
     Landmark(name: "外滩", enName: "Bund", location: "上海"),
     Landmark(name: "劝业场", enName: "QuanYeChang", location: "天津"),
+    Landmark(name: "布达拉宫", enName: "Potala Palace", location: "西藏拉萨"),
+    Landmark(name: "长城", enName: "Great Wall", location: "北京"),
+    Landmark(name: "洪崖洞", enName: "HongYa hole", location: "重庆"),
+    Landmark(name: "龙象塔", enName: "Dragon like towers", location: "广西南宁"),
+    Landmark(name: "大召寺", enName: "Big called temple", location: "内蒙古呼和浩特"),
+    Landmark(name: "外滩", enName: "Bund", location: "上海"),
+    Landmark(name: "劝业场", enName: "QuanYeChang", location: "天津"),
     Landmark(name: "布达拉宫", enName: "Potala Palace", location: "西藏拉萨")
 ]
 
